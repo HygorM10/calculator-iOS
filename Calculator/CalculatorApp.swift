da2668fc61b4d30b0197f9d1c10010e8c304bec1
Hygor Martins <hygor.martins74@gmail.com>
2022-02-12 21:06:50 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Hygor Martins on 12/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
